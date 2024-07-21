7af9399e22fbf9e94ac6ef6cb7fbbe9793d92132
jay shankar <jayshankarprasad57@gmail.com>
2024-07-21 19:22:10 +0530

    For Loop while do while and function

---
+++ src/ex_21072024/Lab115.java
@@ -0,0 +1,19 @@
+package ex_21072024;
+
+public class Lab115 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1 ; i--) {
+            System.out.println(i);
+        }
+        // 10
+        //9
+        //8
+        //7
+        //6
+        //5
+        //4
+        //3
+        //2
+        //1
+    }
+}
