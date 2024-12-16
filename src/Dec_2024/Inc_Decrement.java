3bf3af2cd47b93c1a94077331fba21554215d80e
jay shankar <jayshankarprasad57@gmail.com>
2024-12-17 01:24:11 +0530

    Practice

---
+++ src/Dec_2024/Inc_Decrement.java
@@ -0,0 +1,20 @@
+package Dec_2024;
+
+public class Inc_Decrement {
+    public static void main(String[] args) {
+       /* int a =10;
+        int b=11;
+        System.out.println("Value of A is " + a++);
+        System.out.println("Value of B is " + ++b);
+        System.out.println("Value of A+B is " + a++ + ++b);*/
+        int i = 0;
+        //int j = i++ + ++i;
+        //System.out.println("Value of i: " + i++);
+        //System.out.println("Value of j: " + ++j);
+        //System.out.println(" i: " + i + ",j: " +j);
+        while(i <10){
+            System.out.println(i++);
+        }
+
+    }
+}
---
+++ src/Dec_2024/Switch_Case.java
@@ -0,0 +1,30 @@
+package Dec_2024;
+
+public class Switch_Case {
+    public static void main(String[] args) {
+        /*String color = "red";
+        switch(color){
+            case "red":
+                System.out.println("Red color");
+                break;
+            case "Blue":
+                System.out.println("Blue color");
+                break;
+            default:
+                System.out.println("No Color");
+                break;
+        }*/
+        int day = 1;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+}
